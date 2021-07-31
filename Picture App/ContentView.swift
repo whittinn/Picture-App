2be70e3ceb298c5f358a3d56022222c9414a2083
Nathaniel Whittington <whittinn@gmail.com>
2021-07-31 10:56:11 -0500

    Initial Commit

---
+++ Picture App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Picture App
+//
+//  Created by Nathaniel Whittington on 7/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
